03238fb3436468d4ae4b798b9565de88dd18855b
AnasFarid <anasfaridsamoo@gmail.com>
2026-01-29 18:23:05 +0500

    Initial commit

---
+++ catalog-service/src/test/java/com/demo/catalogservice/TestcontainersConfiguration.java
@@ -0,0 +1,18 @@
+package com.demo.catalogservice;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgresContainer() {
+        //postgres:16-alpine is used here to match the production database version
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"));
+    }
+}
