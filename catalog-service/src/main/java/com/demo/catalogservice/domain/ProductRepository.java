f72c9a5a29ef8dba00b81671c695afb02c8b8484
AnasFarid <anasfaridsamoo@gmail.com>
2026-02-01 16:16:25 +0500

    fix git action

--- catalog-service/src/main/java/com/demo/catalogservice/domain/ProductRepository.java
+++ catalog-service/src/main/java/com/demo/catalogservice/domain/ProductRepository.java
@@ -4,6 +4,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface ProductRepository extends JpaRepository<Product, Long> {
-
+    // find by code
     Optional<Product> findByCode(String code);
 }
